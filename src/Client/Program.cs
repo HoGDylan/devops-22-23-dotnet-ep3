9808b3980c8be707df3af41e7781e70ffa7e5fd6
HoGDylan <dylan.rathe@student.hogent.be>
2022-10-24 13:30:20 +0200

    Sidepanel Toegevoegd, tijdelijk met Counter in

--- src/Client/Program.cs
+++ src/Client/Program.cs
@@ -1,12 +1,14 @@
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using Client;
+using Append.Blazor.Sidepanel;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+builder.Services.AddSidepanel();
 
 await builder.Build().RunAsync();
 //voor te committen...
