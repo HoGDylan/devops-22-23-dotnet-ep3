4dac3821f7b01705b0a19f6634df8f907648e5b1
Arthur Libberecht <arthurlibberecht@student.hogent.be>
2022-11-02 13:51:11 +0100

    klein confilct

--- src/Shared/VirtualMachines/VirtualMachineDto.cs
+++ src/Shared/VirtualMachines/VirtualMachineDto.cs
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 ﻿using Domain.Common;
 using Domain.Contract;
 using Domain.Users;
@@ -46,28 +45,3 @@ public static class VirtualMachineDto
 
     }
 }
-=======
-﻿using Domain.Common;
-using Domain.VirtualMachines;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace Shared.VirtualMachines;
-
-public static class VirtualMachineDto
-{
-    public class Index
-    {
-        public int Id { get; set; }
-        public string Name { get; set; }
-        public VirtualMachineMode mode;
-    }
-    public class Detail : Index
-    {
-        //public Hardware 
-    }
-}
->>>>>>> layoutVM
