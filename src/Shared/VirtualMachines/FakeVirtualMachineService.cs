29825d9c52e8020529ce1857654307a8d3cd690e
DarioBronders <dariobronders123@hotmail.com>
2022-11-05 13:13:49 +0100

    resolved conflict

--- src/Shared/VirtualMachines/FakeVirtualMachineService.cs
+++ src/Shared/VirtualMachines/FakeVirtualMachineService.cs
@@ -11,7 +11,6 @@ namespace Shared.VirtualMachines
     public class FakeVirtualMachineService : IVirtualMachineService
     {
 
-// MAIN
         private static readonly List<VirtualMachine> _virtualMachines = new();
 
         static FakeVirtualMachineService()
@@ -62,60 +61,5 @@ namespace Shared.VirtualMachines
         {
             throw new NotImplementedException();
         }
-
-
-/*
-        private static readonly List<VirtualMachine> _virtualMachines = new();
-
-        static FakeVirtualMachineService()
-        {
-            var vmFaker = new VirtualMachineFaker();
-
-            _virtualMachines = vmFaker.Generate(25);
-        }
-
-        public Task CreateAsync(VirtualMachineResponse.Create request)
-        {
-            throw new NotImplementedException();
-        }
-
-        public Task DeleteAsync(VirtualMachineResponse.Delete request)
-        {
-            throw new NotImplementedException();
-        }
-
-        public Task Edit(VirtualMachineResponse.Edit request)
-        {
-            throw new NotImplementedException();
-        }
-
-        public async Task<VirtualMachineResponse.GetDetail> GetDetailAsync(VirtualMachineRequest.GetDetail request)
-        {
-            await Task.Delay(100);
-            VirtualMachineResponse.GetDetail response = new();
-            response.VirtualMachine = _virtualMachines.Select(e => new VirtualMachineDto.Detail
-            {
-                Id = e.Id,
-                Name = e.Name,
-                BackUp = e.BackUp,
-                Connection = e.Connection,
-                Contract = e.Contract,
-                Hardware = e.Hardware,
-                Mode = e.Mode,
-                OperatingSystem = e.OperatingSystem,
-                Project = e.Project,
-                Klant = e.Customer
-
-            }).SingleOrDefault(f => f.Id == request.VirtualMachineId);
-
-            return response;
-
-        }
-
-        public Task<IEnumerable<VirtualMachineResponse.GetIndex>> GetIndexAsync(VirtualMachineRequest.GetIndex request)
-        {
-            throw new NotImplementedException();
-        }
-*/
     }
 }
