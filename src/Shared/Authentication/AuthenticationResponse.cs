f9876de466865595beef7a32ae934a973f242baf
DarioBronders <dariobronders123@hotmail.com>
2022-11-10 23:57:26 +0100

    forgot register

--- src/Shared/Authentication/AuthenticationResponse.cs
+++ src/Shared/Authentication/AuthenticationResponse.cs
@@ -16,6 +16,7 @@
                 //idk hoe we dit zien in de les, maar dacht we werkten met 0auth?
                 // eventueel hier die 0auth token respond teruggeven, of eventueel de user om die te cachen in front end.
 
+                //public Gebruiker User {get;set;}
             }
         }
 
@@ -24,11 +25,16 @@
             public class Any { }
             public class Failed : Any
             {
+                public Exception? Error { get; set; }
+                public string Message { get; set; }
 
             }
             public class Success : Any
             {
+                //idk hoe we dit zien in de les, maar dacht we werkten met 0auth?
+                // eventueel hier die 0auth token respond teruggeven, of eventueel de user om die te cachen in front end.
 
+                //public Gebruiker User {get;set;}
             }
         }
 
