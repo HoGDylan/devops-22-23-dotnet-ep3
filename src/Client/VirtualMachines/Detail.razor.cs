d1d410c49376a08c8f3960f5c2a43d016b5e678c
Arthur Libberecht <arthurlibberecht@student.hogent.be>
2022-11-17 21:10:09 +0100

    detailspagina vm

--- src/Client/VirtualMachines/Detail.razor.cs
+++ src/Client/VirtualMachines/Detail.razor.cs
@@ -2,6 +2,7 @@ using Microsoft.AspNetCore.Components;
 using Shared.VirtualMachines;
 using Client.VirtualMachines.Components;
 using Domain.VirtualMachines.VirtualMachine;
+using Client.Shared;
 
 namespace Client.VirtualMachines;
 
@@ -9,6 +10,7 @@ public partial class Detail
 {
     public VirtualMachineDto.Detail Virtualmachine { get; set; }
     [Inject] public IVirtualMachineService vmService { get; set; }
+    [Inject] NavigationManager NavMan { get; set; }
     [Parameter] public int Id { get; set; }
 
 
@@ -20,5 +22,15 @@ public partial class Detail
         var vm_request = await vmService.GetDetailAsync(request);
         Virtualmachine = vm_request.VirtualMachine;
         Console.WriteLine(Virtualmachine.Hardware.ToString());
+    }
+
+    private void NavigateToKlant()
+    {
+        NavMan.NavigateTo($"/virtualmachine{Virtualmachine.Id}/Klant");
+    }
+
+    public void NavigateToReport()
+    {
+        NavMan.NavigateTo($"virtualmachine/rapport/{Virtualmachine.Id}");
     }
 }
