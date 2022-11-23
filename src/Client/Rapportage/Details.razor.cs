88bdd7d033125e9d70f1e6f8e7c7791ede725c1a
Arthur Libberecht <arthurlibberecht@student.hogent.be>
2022-11-23 18:13:32 +0100

    details page servers

--- src/Client/Rapportage/Details.razor.cs
+++ src/Client/Rapportage/Details.razor.cs
@@ -8,14 +8,14 @@ namespace Client.Rapportage;
 
 partial class Details
 {
+    public string KeuzeChart { get; set; }
+    public string Model { get; set; }
     string[] chartFilter = new string[] { "Per Uur", "Dagelijks", "Wekenlijks", "Maandenlijks" };
     [Parameter] public int Id { get; set; }
-    public string Model { get; set; }
+    
     [Inject] IFysiekeServerService Service { get; set; }
-    public string KeuzeChart { get; set; }
     private FysiekeServerDto.Detail server_response;
-    ExtendedChartData _lineData { get; set; }
-    LineOptions _defaultOptions { get; set; } = new();
+
     protected override async Task OnInitializedAsync()
     {
         FysiekeServerRequest.Detail request = new FysiekeServerRequest.Detail() { ServerId = Id };
