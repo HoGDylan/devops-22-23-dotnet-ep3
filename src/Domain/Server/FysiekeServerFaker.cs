f7b601903af6171b5e80e5a50550874ab9399161
DarioBronders <dariobronders123@hotmail.com>
2022-11-23 15:47:22 +0100

    bugfix

--- src/Domain/Server/FysiekeServerFaker.cs
+++ src/Domain/Server/FysiekeServerFaker.cs
@@ -3,35 +3,35 @@ using Domain.Common;
 using Domain.VirtualMachines;
 using Domain.VirtualMachines.VirtualMachine;
 using System;
+using System.Security.Cryptography;
 
 namespace Domain.Server
 {
     public class FysiekeServerFaker : Faker<FysiekeServer>
     {
-        private static readonly int[] _memoryOptions = { 1024, 2048, 4096, 8192, 16384 };
-        private static readonly int[] _storageOptions = { 1000, 2000, 5000, 10000, 20000 };
-
-        private List<Hardware> _hardWareOptions = GenerateRandomHardware();
         public FysiekeServerFaker()
         {
             int id = 1;
 
-            CustomInstantiator(e => new FysiekeServer("Server " + id, e.PickRandom(_hardWareOptions), e.Internet.DomainName() + "." + "hogent.be"));
+            CustomInstantiator(e => new FysiekeServer("Server " + id, GenerateRandomHardware(), e.Internet.DomainName() + "." + "hogent.be"));
             RuleFor(e => e.Id, _ => id++);
             RuleFor(e => e.VirtualMachines, _ => VirtualMachineFaker.Instance.Generate(10));
             
         }
 
 
-        private static List<Hardware> GenerateRandomHardware()
+        private Hardware GenerateRandomHardware()
         {
+            int[] _memoryOptions = { 1024, 2048, 4096, 8192, 16384 };
+            int[] _storageOptions = { 1000, 2000, 5000, 10000, 20000 };
             List<Hardware> res = new();
 
             for (int i = 0; i < 100; i++)
             {
-                res.Append(new Hardware(_memoryOptions[new Random().Next(0, _memoryOptions.Count())], _storageOptions[new Random().Next(0, _storageOptions.Count())], new Random().Next(50, 200)));
+                res.Add(new Hardware(_memoryOptions[new Random().Next(0, _memoryOptions.Count())], _storageOptions[new Random().Next(0, _storageOptions.Count())], new Random().Next(50, 200)));
             }
-            return res;
+
+            return res[RandomNumberGenerator.GetInt32(0, res.Count())];
         }
     }
 }
--- src/Domain/VirtualMachines/VirtualMachine/VirtualMachineFaker.cs
+++ src/Domain/VirtualMachines/VirtualMachine/VirtualMachineFaker.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Net;
+using System.Reflection.Metadata.Ecma335;
 using System.Security.Cryptography;
 using System.Text;
 using System.Threading.Tasks;
@@ -24,28 +25,27 @@ namespace Domain.VirtualMachines.VirtualMachine
 
 
 
-        private static readonly int[] _memoryOptions = { 1, 2, 4, 8, 16, 32, 64 };
-        private static readonly int[] _storageOptions = { 30, 50, 75, 100, 150, 200, 250, 300, 500 };
 
         //private readonly IEnumerable<DateTime?> _dateOptions = GenerateRandomDatesIncNull();
-        private readonly List<Hardware> _hardWareOptions = GenerateRandomHardware();
-        private readonly List<Backup> _backupOptions = GenerateRandomBackups();
-
 
 
         private List<VirtualMachine> _virtualMachines = new();
 
 
         private static VirtualMachineFaker instance = null;
-    
-        public static VirtualMachineFaker Instance { get {
+
+        public static VirtualMachineFaker Instance
+        {
+            get
+            {
                 if (instance == null)
                 {
                     instance = new VirtualMachineFaker();
                 }
                 return instance;
-            } }
-        
+            }
+        }
+
 
         public VirtualMachineFaker()
         {
@@ -54,15 +54,15 @@ namespace Domain.VirtualMachines.VirtualMachine
             CustomInstantiator(e => new VirtualMachine(
                 e.Commerce.ProductName(),
                 e.PickRandom<OperatingSystemEnum>(),
-                e.PickRandom(_hardWareOptions),
-                e.PickRandom(_backupOptions)
+                e.PickRandom(GenerateRandomHardware(false)),
+                e.PickRandom(GenerateRandomBackups())
                 ));
 
             RuleFor(x => x.Id, _ => id++);
             RuleFor(x => x.Connection, _ => new Random().Next(0, 2) % 1 == 0 ? new VMConnection("MOCK-FQDN", GetRandomIpAddress(), "MOCK-USER", PasswordGenerator.Generate(20, 3, 3, 3, 3)) : null);
             RuleFor(x => x.Mode, x => x.PickRandom<VirtualMachineMode>());
             RuleFor(x => x.Contract, _ => VMContractFaker.Instance.GenerateOne());
-            //RuleFor(x => x.FysiekeServer, _ => new FysiekeServer("Mock Server", _hardWareOptions[0], "mock-server_adres.hogent.be"));  geeft error
+            //RuleFor(x => x.FysiekeServer, _ => new FysiekeServer("Mock Server", GenerateRandomHardware(true), "mock-server_adres.hogent.be"));
 
         }
 
@@ -83,7 +83,7 @@ namespace Domain.VirtualMachines.VirtualMachine
             }
             else
             {
-     
+
                 output = _virtualMachines.GetRange(0, count);
 
             }
@@ -91,63 +91,56 @@ namespace Domain.VirtualMachines.VirtualMachine
         }
 
 
-        private static List<DateTime?> GenerateRandomDatesIncNull()
+        private static DateTime? GenerateRandomDatesIncNull()
         {
             List<DateTime?> res = new();
             DateTime min = new DateTime(2022, 09, 01);
 
-            for (int i = 0; i < 100; i++)
+            for (int i = 0; i < 10; i++)
             {
                 res.Add(min.AddDays(new Random().Next((DateTime.Now - min).Days)));
             }
             res.Add(null);
 
-            return res;
+            return res[RandomNumberGenerator.GetInt32(0, res.Count())];
 
 
         }
 
-        private static List<Hardware> GenerateRandomHardware()
+        private Hardware GenerateRandomHardware(bool server)
         {
             List<Hardware> res = new();
 
-            for (int i = 0; i < 100; i++)
-            {
-                res.Add(new Hardware(_memoryOptions[new Random().Next(0, _memoryOptions.Count())], _storageOptions[new Random().Next(0, _storageOptions.Count())], new Random().Next(1, 13)));
-            }
-            return res;
+
+            int[] _memoryOptions = { 1, 2, 4, 8, 16, 32, 64 };
+            int[] _storageOptions = { 30, 50, 75, 100, 150, 200, 250, 300, 500 };
+
+
+            return new Hardware(_memoryOptions[new Random().Next(0, _memoryOptions.Count())] * (server ? 15 : 1), _storageOptions[new Random().Next(0, _storageOptions.Count())] * (server ? 15 : 1), new Random().Next(1, 13) * (server ? 15 : 1));
         }
 
-        private static List<Backup> GenerateRandomBackups()
+        private Backup GenerateRandomBackups()
         {
             List<Backup> res = new();
 
-            for (int i = 0; i < 100; i++)
-            {
-                int r = new Random().Next(0, 10);
-                Backup a;
+            int r = new Random().Next(0, 10);
+            Backup a;
+
+            if (r == 1)
+                a = new Backup(BackUpType.DAILY, DateTime.Now.Subtract(TimeSpan.FromMinutes(500)));
+            else if (r == 2)
+                a = new Backup(BackUpType.CUSTOM, DateTime.Now.Subtract(TimeSpan.FromHours(5)));
+            else if (r <= 6)
+                a = new Backup(BackUpType.WEEKLY, DateTime.Now.Subtract(TimeSpan.FromDays(new Random().NextDouble() * 7)));
+            else
+                a = new Backup(BackUpType.MONTHLY, DateTime.Now.Subtract(TimeSpan.FromDays(new Random().Next(30))));
 
 
-
-                if (r == 1)
-                    a = new Backup(BackUpType.DAILY, DateTime.Now.Subtract(TimeSpan.FromMinutes((i + 1) * 20)));
-                else if (r == 2)
-                    a = new Backup(BackUpType.CUSTOM, DateTime.Now.Subtract(TimeSpan.FromHours((i + 1) * 50)));
-                else if (r <= 6)
-                    a = new Backup(BackUpType.WEEKLY, DateTime.Now.Subtract(TimeSpan.FromDays(new Random().NextDouble() * 7)));
-                else
-                    a = new Backup(BackUpType.MONTHLY, DateTime.Now.Subtract(TimeSpan.FromDays(new Random().Next(30))));
-
-
-                res.Add(a);
-
-
-            }
-            return res;
+            return a;
 
         }
 
-        private static IPAddress GetRandomIpAddress()
+        private IPAddress GetRandomIpAddress()
         {
             var random = new Random();
             var data = new byte[4];
