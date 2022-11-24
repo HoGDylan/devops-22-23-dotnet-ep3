8ec7508000e036f7fa8e80237e092b1e25d407f1
DarioBronders <dariobronders123@hotmail.com>
2022-11-24 15:02:53 +0100

    add extensionmethod

--- src/Domain/VirtualMachines/BackUp/BackupType.cs
+++ src/Domain/VirtualMachines/BackUp/BackupType.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using Domain.VirtualMachines.BackUp;
+using Domain.VirtualMachines.VirtualMachine;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -14,3 +16,25 @@ namespace Domain.VirtualMachines.BackUp
         MONTHLY,
     }
 }
+
+
+    public static class Format
+    {
+        public static String GetString(this BackUpType type)
+        {
+            switch (type)
+            {
+                case BackUpType.CUSTOM:
+                    return "Custom";
+                case BackUpType.DAILY:
+                    return "Dagelijks";
+                case BackUpType.WEEKLY:
+                    return "Wekelijks";
+                case BackUpType.MONTHLY:
+                    return "Maandelijks";
+
+                default: throw new ArgumentException("No Case for: " + type.ToString());
+
+            }
+        }
+    }
