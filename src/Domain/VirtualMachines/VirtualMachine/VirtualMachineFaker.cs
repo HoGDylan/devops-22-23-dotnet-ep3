a660fb6c393009cb75b847a5e8a1bb8634756bc3
DarioBronders <dariobronders123@hotmail.com>
2022-11-26 13:18:23 +0100

    fixed memory heap issue by using singleton instead of creating instances for each VM

--- src/Domain/VirtualMachines/VirtualMachine/VirtualMachineFaker.cs
+++ src/Domain/VirtualMachines/VirtualMachine/VirtualMachineFaker.cs
@@ -68,8 +68,9 @@ namespace Domain.VirtualMachines.VirtualMachine
             RuleFor(x => x.Mode, x => x.PickRandom<VirtualMachineMode>());
             RuleFor(x => x.Contract, _ => contract);
             RuleFor(x => x.FysiekeServer, _ => new FysiekeServer("Mock Server", FysiekeServerFaker.GenerateRandomHardware(), "mock-server_adres.hogent.be"));
-            
-            //RuleFor(x => x.Statistics, _ => new Statistic(contract.StartDate, contract.EndDate, hardware));  bugs de shit out of VM List
+
+
+            RuleFor(x => x.Statistics, _ => new Statistic(contract.StartDate, contract.EndDate, hardware));
         }
 
 
